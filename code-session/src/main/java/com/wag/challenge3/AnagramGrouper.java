bb79a400704cf41370a801da2facf036dcaa970c
eriskooo <lorman@pobox.sk>
2025-12-23 14:24:39 +0100

    buble

--- code-session/src/main/java/com/wag/challenge3/AnagramGrouper.java
+++ code-session/src/main/java/com/wag/challenge3/AnagramGrouper.java
@@ -16,6 +16,53 @@ public class AnagramGrouper {
      */
     public List<String> groupAnagrams(String word, List<String> words) {
         // TODO: Your implementation
-        return null;
+        List<String> output = new ArrayList<>();
+
+        for (String temp : words) {
+            if (hasAll(word, temp)) {
+                output.add(temp);
+            }
+        }
+        // sort alphabetically
+        System.out.println(output);
+
+
+        return sorted(output);
+    }
+
+    private List<String> sorted(List<String> output) {
+        int n = output.size();
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                String left = output.get(j);
+                String right = output.get(j + 1);
+                if (left.compareTo(right) > 0) {
+                    swapped = true;
+                    output.set(j, right);
+                    output.set(j + 1, left);
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+        return output;
+    }
+
+    private boolean hasAll(String word, String temp) {
+        for (char ch : word.toCharArray()) {
+            boolean contains = false;
+            for (char ch2 : temp.toCharArray()) {
+                if (ch == ch2) {
+                    contains = true;
+                }
+            }
+            if (!contains) {
+                return contains;
+            }
+        }
+        return true;
     }
 }
