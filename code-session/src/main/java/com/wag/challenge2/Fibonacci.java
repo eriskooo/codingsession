6244c1d2e57c593bd7f7d2658ccfbaaae9a18690
eriskooo <lorman@pobox.sk>
2025-12-23 13:26:15 +0100

    Fibonacci

--- code-session/src/main/java/com/wag/challenge2/Fibonacci.java
+++ code-session/src/main/java/com/wag/challenge2/Fibonacci.java
@@ -4,22 +4,43 @@ public class Fibonacci {
 
     /**
      * Find nth fibonacci number.
-     *
+     * <p>
      * Fibonacci sequence is defined as follows:
      * F(0) = 0
      * F(1) = 1
      * F(n) = F(n-1) + F(n-2)
      * Example of sequence: 0, 1, 1, 2, 3, 5, 8....
      * Example F(6) = 8
-     *
+     * <p>
      * What is wrong with following implementation?
-     *
+     * <p>
      * Change it so all unit tests are passed.
      */
     public static long fibonacci(long n) {
-        if (n <= 1) {
-            return n;
+        if (n < 0) {
+            throw new IllegalArgumentException();
         }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        // return fibonacci(n - 1) + fibonacci(n - 2);
+
+        Long l1 = 1l;
+        Long l2 = 1l;
+        System.out.println("n = " + n);
+        System.out.println("=========================================");
+        for (int i = 2; i < n; i++) {
+            long temp = l2;
+            l2 = l1 + l2;
+            l1 = temp;
+
+            System.out.println("l1 = " + l1);
+            System.out.println("l2 = " + l2);
+        }
+
+        return l2;
     }
 }
