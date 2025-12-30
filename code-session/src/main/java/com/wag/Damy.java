5edf263d6913b9e357e24eac6755441f4fa56626
eriskooo <lorman@pobox.sk>
2025-12-30 15:32:12 +0100

    damy

--- code-session/src/main/java/com/wag/Damy.java
+++ code-session/src/main/java/com/wag/Damy.java
@@ -3,7 +3,7 @@ package com.wag;
 public class Damy {
 
     static int[][] pole = {
-            {0, 2, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 0},
             {1, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0},
@@ -15,6 +15,28 @@ public class Damy {
 
     public static void main(String[] args) {
         kresli(pole);
+        boolean ok = validuj(pole);
+        System.out.println(ok);
+    }
+
+    private static boolean validuj(int[][] pole) {
+        // horizontaly
+        for (int i = 0; i < pole.length; i++) {
+            for (int j = 0; j < pole.length; j++) {
+                if (pole[i][j] != 0) {
+                    boolean b = pozriSmery(pole, i, j);
+                    if (b == false) {
+                        System.out.println("fail at i=" + i + ", j=" + j);
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean pozriSmery(int[][] pole, int i, int j) {
+        return false;
     }
 
     private static void kresli(int[][] pole) {
@@ -26,3 +48,5 @@ public class Damy {
         }
     }
 }
+
+
