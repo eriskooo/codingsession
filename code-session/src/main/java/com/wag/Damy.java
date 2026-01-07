b0acf6672876b83e05124deea29f2f890eaf8791
lorman erich <lorman.erich@scheidt-bachmann.sk>
2026-01-07 11:33:32 +0100

    vzdal som pole a vnorene iteracie

--- code-session/src/main/java/com/wag/Damy.java
+++ code-session/src/main/java/com/wag/Damy.java
@@ -18,18 +18,78 @@ public class Damy {
             {0, 0, 0, 0, 0, 0, 0, 0}
     };
 
+    static int[] colForRow = {-1, -1, -1, -1, -1, -1, -1, -1};
+
     public static void main(String[] args) {
-        int counter = 0;
-        for (int i = 0; i < pole.length; i++) {
-            for (int j = 0; j < pole[i].length; j++) {
-                boolean found = horizontal.test(pole, i, j) || vertical.test(pole, i, j) || diagonal.test(pole, i, j);
-                if (!found) {
-                    pole[i][j] = ++counter;
-                    break;
-                }
+        int row=0;
+        while (row >= 0 && row < 8) {
+            int nextCol =  hladajRexo(row, colForRow);
+            if (nextCol != -1) {
+                // našli sme bezpečný stĺpec – umiestnime dámu
+                colForRow[row] = nextCol;
+                // ideme do ďalšieho riadku
+                row++;
+            } else {
+                // v tomto riadku už niet kam dať dámu – musíme sa vrátiť o riadok späť
+                colForRow[row] = -1; // tento riadok je aktuálne bez dámy
+                row--;               // backtracking
             }
         }
-        kresli(pole);
+        if (row == 8) {
+            // máme riešenie – vypíšeme šachovnicu
+            printBoard(colForRow);
+        } else {
+            System.out.println("Žiadne riešenie sa nenašlo.");
+        }
+    }
+
+    private static int hladajRexo(int row, int[] colForRow) {
+        int startCol = colForRow[row] + 1;
+
+        for (int col = startCol; col < 8; col++) {
+            if (isSafe(row, col, colForRow)) {
+                return col;
+            }
+        }
+        return -1; // v tomto riadku už niet žiadneho bezpečného stĺpca
+    }
+
+    /**
+     * Overí, či je bezpečné umiestniť dámu na (row, col),
+     * ak v riadkoch 0..row-1 už dámu máme podľa colForRow.
+     */
+    private static boolean isSafe(int row, int col, int[] colForRow) {
+        for (int r = 0; r < row; r++) {
+            int c = colForRow[r];
+
+            // rovnaký stĺpec
+            if (c == col) {
+                return false;
+            }
+
+            // diagonála: rozdiel riadkov == rozdiel stĺpcov
+            if (Math.abs(row - r) == Math.abs(col - c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Pomocná metóda na vykreslenie riešenia.
+     * 'Q' = dáma, '.' = prázdne pole.
+     */
+    private static void printBoard(int[] colForRow) {
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                if (colForRow[row] == col) {
+                    System.out.print("Q ");
+                } else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     static TriPredicate<int[][], Integer, Integer> horizontal = (board, row, col) -> {
@@ -89,7 +149,7 @@ public class Damy {
     static void kresli(int[][] pole) {
         for (int i = 0; i < pole.length; i++) {
             for (int j = 0; j < pole.length; j++) {
-                System.out.print(pole[i][j] +"\t");
+                System.out.print(pole[i][j] + "\t");
             }
             System.out.println();
         }
