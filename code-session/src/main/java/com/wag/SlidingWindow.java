2026fb0c934c56fbab4d0b272506bc9aff9c3c5c
lorman erich <lorman.erich@scheidt-bachmann.sk>
2026-01-13 10:40:56 +0100

    SlidingWindow

--- code-session/src/main/java/com/wag/SlidingWindow.java
+++ code-session/src/main/java/com/wag/SlidingWindow.java
@@ -1,5 +1,7 @@
 package com.wag;
 
+import java.util.Arrays;
+
 public class SlidingWindow {
 
     /**
@@ -10,7 +12,33 @@ public class SlidingWindow {
      * @return
      */
     static int maxSumWindow(int[] a, int w) {
-        return -1;
+        if (a == null || a.length < w) {
+            throw new IllegalArgumentException();
+        }
+
+        if (w < 1) {
+            throw new IllegalArgumentException();
+        }
+
+        System.out.println("a = " + Arrays.toString(a));
+        System.out.println("w = " + w);
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i <= a.length - w; i++) {
+            int temp = 0;
+
+            // spocitame vsetko pre okno
+            for (int j = 0; j < w; j++) {
+                temp = temp + a[i + j];
+            }
+
+            if (temp > max) {
+                max = temp;
+            }
+        }
+
+        System.out.println("max = " + max);
+        return max;
     }
 
 }
